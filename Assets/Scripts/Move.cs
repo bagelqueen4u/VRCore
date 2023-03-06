a0452bf57eff6d311b639b65db5aec278c43ec5b
bagelqueen4u <dh3225@drexel.edu>
2023-03-06 17:45:37 -0500

    Integrated Arduino with Unity via a simple Cube that moves left on capacitive touch and right otherwise

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // change your serial port
+    SerialPort sp = new SerialPort("COM3", 9600);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 100; // In my case, 100 was a good amount to allow quite smooth transition. 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sp.IsOpen)
+        {
+            try
+            {
+                // When left button is pushed
+                if (sp.ReadByte() == 1)
+                {
+                    print(sp.ReadByte());
+                    transform.Translate(Vector3.left * Time.deltaTime * 5);
+                }
+                // When right button is pushed
+                if (sp.ReadByte() == 2)
+                {
+                    print(sp.ReadByte());
+                    transform.Translate(Vector3.right * Time.deltaTime * 5);
+                }
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+        }
+    }
+}
